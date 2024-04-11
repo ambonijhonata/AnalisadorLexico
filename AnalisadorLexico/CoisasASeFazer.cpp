466859650de179ded17389bb81eaa29d1a044499
ambonijhonata <jhonata01amboni@gmail.com>
2024-04-11 19:25:12 -0300

    criado código para reconhecer nome de variaveis

---
+++ AnalisadorLexico/CoisasASeFazer.cpp
@@ -0,0 +1,2 @@
+//1 - classificar o nome da variável, exemplo: nome: int;. Nesse caso o lexema nome seria um nomevariavel, ai tem que mandar o código desse cara na gramatica
+//2- Verificar para limpar o lexema nos tokens tipo "[]{},;.: e etc. Nesses casos não pode quebrar a sentença no espaço,
